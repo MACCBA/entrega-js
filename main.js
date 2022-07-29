59b1fdefd611ae7c215d32f1417d1724e9f542e2
MACCBA <mac88_13@hotmail.com>
2022-07-28 23:22:53 -0300

    first

---
+++ main.js
@@ -0,0 +1,70 @@
+const Pizzas= [
+    {
+      id: 1,
+      nombre: 'de Mozzarella',
+      precio: 590,
+      Ingredientes: ['Queso', 'Salsa', ],
+    },
+    {
+      id: 2,
+      nombre: 'Especial',
+      precio: 800,
+      Ingredientes: ['Queso', 'Salsa', 'Jamon'],
+    },
+    {
+      id: 3,
+      nombre: 'Napolitana',
+      precio:  1000,
+      Ingredientes: ['Queso', 'Salsa', 'Jamon', 'Tomate'],
+    },
+    {
+      id: 4,
+      nombre: 'Fugazza',
+      precio: 1100,
+      Ingredientes: ['Queso', 'Cebolla'],
+    },
+    {
+      id: 5,
+      nombre: 'de Champignones',
+      precio: 1200,
+      Ingredientes: ['Queso', 'Champignones', 'Jamon' ],
+    },
+    {
+      id: 6,
+      nombre: 'de Cuatro Quesos',
+      precio: 1200,
+      Ingredientes: ['Queso Azul', 'Parmesano', 'Cremoso', 'Mozzarella'],
+    },
+  ];
+
+// a=   
+        // const imprimirImpar = Pizzas.filter((id))
+       
+       
+
+// b=
+        //  const menoresA600 = Pizzas.filter((producto) => 
+        //  producto.precio < 600);
+
+        //  console.log(menoresA600);
+   
+// c= 
+    //   const imprimirNombre = () =>
+    //    Pizzas.forEach((producto) =>
+    //    console.log(`Pizza ${producto.nombre}`));
+
+    //    imprimirNombre();
+
+// d=
+    //    const imprimirPrecio = () =>
+    //     Pizzas.forEach((producto) =>
+    //     console.log(`Los precios son ${producto.precio}`));
+
+    //      imprimirPrecio();
+
+// e=
+    //   const imprimirNombreyPrecio = () =>
+    //    Pizzas.forEach((producto) =>
+    //    console.log(`La Pizza ${producto.nombre} sale $${producto.precio}`));
+
+    //    imprimirNombreyPrecio();
